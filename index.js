d345b3165e006d427abca623d8e02f91f9f72f6e
Jonathan Nicholas <Jonathan.Nicholas@protonmail.com>
2019-06-06 21:02:02 +1000

    fix: CORS issue

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const postRoute = require("./src/router/post");
 const categoriesRoute = require("./src/router/categories");
 
 const app = express();
+app.use(cors());
 app.use("/posts", postRoute);
 app.use("/categories", categoriesRoute);
 app.listen(process.env.PORT);
